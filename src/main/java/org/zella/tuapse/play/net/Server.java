afd42b94292e38a68800067312f4239c331661c6
zella <drumirage@gmail.com>
2019-06-13 20:33:20 +0300

    fix ci, allow cross domain requests

--- src/main/java/org/zella/tuapse/play/net/Server.java
+++ src/main/java/org/zella/tuapse/play/net/Server.java
@@ -4,11 +4,13 @@ package org.zella.tuapse.play.net;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.core.http.HttpServer;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
 import io.vertx.reactivex.ext.web.handler.BodyHandler;
+import io.vertx.reactivex.ext.web.handler.CorsHandler;
 import io.vertx.reactivex.ext.web.handler.StaticHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,8 @@ import org.zella.tuapse.play.model.net.PlayInput;
 import org.zella.tuapse.play.utils.Json;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Server {
 
@@ -36,6 +40,23 @@ public class Server {
     public Single<HttpServer> create() {
 
         Router router = Router.router(Vertx.vertx());
+
+        Set<String> allowedHeaders = new HashSet<>();
+        allowedHeaders.add("x-requested-with");
+        allowedHeaders.add("Access-Control-Allow-Origin");
+        allowedHeaders.add("origin");
+        allowedHeaders.add("Content-Type");
+        allowedHeaders.add("accept");
+        allowedHeaders.add("X-PINGARUNER");
+        Set<HttpMethod> allowedMethods = new HashSet<>();
+        allowedMethods.add(HttpMethod.GET);
+        allowedMethods.add(HttpMethod.POST);
+        allowedMethods.add(HttpMethod.OPTIONS);
+        allowedMethods.add(HttpMethod.DELETE);
+        allowedMethods.add(HttpMethod.PATCH);
+        allowedMethods.add(HttpMethod.PUT);
+
+        router.route().handler(CorsHandler.create("*").allowedHeaders(allowedHeaders).allowedMethods(allowedMethods));
 
         router.get("/").handler(ctx -> ctx.reroute("/files"));
         router.get("/files/*").handler(StaticHandler.create()
